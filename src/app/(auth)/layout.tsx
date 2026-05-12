e94797e09d987ad4118242f10bdb4e59bfddcf7c
lth-1026 <th20001026@gmail.com>
2026-05-12 09:01:24 +0900

    Revert "fix: auth 레이아웃 overflow+justify-center 접근성 버그 수정"
    This reverts commit b6b3712294dbcdf98de368f5c80cef045954f850.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-full overflow-y-auto bg-bg px-4">
-      <div className="flex min-h-full flex-col items-center justify-center py-8">{children}</div>
+    <div className="flex min-h-full flex-col items-center justify-center overflow-y-auto bg-bg px-4">
+      {children}
     </div>
   )
 }
